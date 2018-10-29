389ba92c0bff9cccbce70110eead8b9ce51b2bcc
sunyifeng <9339996@qq.com>
2018-10-30 00:11:29 +0800

    Initial commit

---
+++ ch08-execution-engine/src/main/java/com/sunyf/ch08/Dispatch.java
@@ -0,0 +1,55 @@
+package com.sunyf.ch08;
+
+/**
+ * @program: understanding-the-jvm
+ * @description: 单分派、多分派
+ * @author: Mr.Sun
+ * @create: 2018-10-29 23:56
+ **/
+public class Dispatch {
+
+    // QQ静态类
+    static class QQ {}
+
+    // 360静态类
+    static class _360 {}
+
+    // 父亲艰难抉择，静态类
+    public static class Father {
+
+      public void hardChoice(QQ arg){
+          System.out.println("father choose qq");
+      }
+
+      public void hardChoice (_360 arg) {
+          System.out.println("father choose 360");
+      }
+
+    }
+
+    // 儿子艰难抉择，继承父亲，静态类
+    public static class Son  extends Father{
+
+        public void hardChoice(QQ arg) {
+            System.out.println("son choose qq");
+        }
+
+        public void hardCoice(_360 arg) {
+            System.out.println("son choose 360");
+        }
+
+    }
+
+    // main 方法
+    public static void main(String[] args) {
+        Father father = new Father();
+        Father son = new Son();
+        //
+        father.hardChoice(new _360());
+        son.hardChoice(new QQ());
+
+    }
+
+
+
+    }
